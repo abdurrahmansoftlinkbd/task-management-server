018ec4c693cfd8c754ce806a556361989446f60b
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-02-21 19:50:31 +0600

    task delete based on id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.npqzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,6 +33,12 @@ async function run() {
     app.post("/tasks", async (req, res) => {
       const taskItem = req.body;
       const result = await taskCollection.insertOne(taskItem);
+      res.send(result);
+    });
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
